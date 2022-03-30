d50525d6fd5b5ad3a0791e080b50e100f63ac566
mitchgemma <mitchgemma@gmail.com>
2022-03-30 18:18:07 -0400

    added dog model

---
+++ app/models/dogs.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const { Schema, model } = mongoose
+
+const dogSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    breed: {
+      type: String,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    friendly: {
+      type: Boolean,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = model('Dog', dogSchema)
